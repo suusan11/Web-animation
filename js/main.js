1494a9d12f5ff4cc516fccc2f45b13e86f0fa1f2
suusan11 <gawow11@gmail.com>
2019-09-27 16:00:35 -0700

    set some properties

--- js/main.js
+++ js/main.js
@@ -1,20 +1,52 @@
 /*  Autumn Greeting Card -- js */
 
-(function($){
+(function ($) {
 	'use strict';
 
 	// declare actors here
+	var $backFallingLeaves = $('#brownLeaf, #orangeLeaf, #redLeaf'),
+		$textLine1 = $('.text-line-1'),
+		$textLine2 = $('.text-line-2'),
+		$textGreeting = $('.text-greeting'),
+		$treeLeaves = $('[id^=treeleaf]'),
+		$flooLeaves = $('[id^=floorleaf]'),
+		$bird = $('#Bird'),
+		$birdHat = $bird.find('#BirdHat'),
+		$birdEyes = $bird.find('#leftEye, #rightEye'),
+		$nest = $('#NestAndLeaves'),
+		$tree = $('#tree_trunk');
 
-	// clear stage 
+	// clear stage
+	function clearStage() {
+		var clearTl = new TimelineMax();
 
+		clearTl
+			.set($backFallingLeaves, { autoAlpha: 0 })
+			.set($textLine1, { autoAlpha: 0 })
+			.set($textLine2, { autoAlpha: 0 })
+			.set($textGreeting, { autoAlpha: 0 })
+			.set($treeLeaves, { autoAlpha: 0 })
+			.set($bird, { y: '+=65', autoAlpha: 0 })
+			.set($nest, { autoAlpha: 0 })
+			.set($tree, { autoAlpha: 0 })
+			.set($flooLeaves, { y: '+=275', autoAlpha: 0 })
+		return clearTl;
+	}
 	// enter floor vegetation
 
 	// enter tree
 
 	// enter the greeting text
-	
+
 	// the GO function ...to kick things all off
-	
+	function go() {
+		var masterTl = new TimelineMax();
+
+		masterTl
+			.add(clearStage(), 'scene-clear-stage')
+	}
+
+	go();
 
 })(jQuery);
 
