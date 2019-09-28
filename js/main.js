d85d9dfe72919b253c35e93d6eb447033add7f56
suusan11 <gawow11@gmail.com>
2019-09-27 17:23:57 -0700

    add some timelines

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
 		$textLine2 = $('.text-line-2'),
 		$textGreeting = $('.text-greeting'),
 		$treeLeaves = $('[id^=treeleaf]'),
-		$flooLeaves = $('[id^=floorleaf]'),
+		$floorLeaves = $('[id^=floorleaf]'),
 		$bird = $('#Bird'),
 		$birdHat = $bird.find('#BirdHat'),
 		$birdEyes = $bird.find('#leftEye, #rightEye'),
@@ -30,7 +30,7 @@
 			.set($bird, { y: '+=65', autoAlpha: 0 })
 			.set($nest, { autoAlpha: 0 })
 			.set($tree, { autoAlpha: 0 })
-			.set($flooLeaves, { y: '+=275', onComplete: showContainer });
+			.set($floorLeaves, { y: '+=275', onComplete: showContainer });
 
 
 		function showContainer() {
@@ -40,9 +40,43 @@
 		return clearTl;
 	}
 	// enter floor vegetation
+	function enterFloorVegetation() {
+		var fleavesTl = new TimelineMax();
 
+		fleavesTl
+			.staggerTo($floorLeaves, 1, { y: 0, ease: $backFallingLeaves.easeInOut }, 0.01)
+			.fromTo($tree, 1, { scaleY: 0.2, autoAlpha: 0, transformOrigin: 'center bottom' },
+				{ scaleY: 1, autoAlpha: 1, transformOrigin: 'center bottom', ease: Back.easeInOut })
+			.fromTo($tree, 1, { scaleX: 0.2, autoAlpha: 0, transformOrigin: 'center bottom' },
+				{ scaleX: 1, autoAlpha: 1, transformOrigin: 'center bottom', ease: Back.easeInOut }, '-=0.9');
+		return fleavesTl;
+	}
 	// enter tree
+	function enterTreestuff() {
+		var treeStuffTl = new TimelineMax();
 
+		treeStuffTl
+			.staggerFromTo($treeLeaves, 0.5, { scale: 0.2, autoAlpha: 0, transformOrigin: 'center bottom' },
+				{ scale: 1, autoAlpha: 1, transformOrigin: 'center bottom' }, 0.02)
+			.fromTo($nest, 1, { y: 0, scale: 0.2, autoAlpha: 0, transformOrigin: 'center center' },
+				{ y: '-=15', scale: 1, autoAlpha: 1, transformOrigin: 'center center', ease: Elastic.easeOut }, '+=0.1')
+			.to($nest, 0.3, { y: '+=15', ease: AudioBufferSourceNode.easeOut }, '-=0.2')
+			.add('nest-pop-in')
+			.set($birdHat, { rotation: 12, x: '+=6' })
+			.to($bird, 1.4, { y: '-=39', autoAlpha: 1, ease: Power4.easeInOut }, 'nest-pop-in+=0.1')
+			.add('bird-peeking')
+			.set($birdEyes, { autoAlpha: 0 })
+			.set($birdEyes, { autoAlpha: 1 }, '+=0.2')
+			.set($birdEyes, { autoAlpha: 0 }, '+=0.3')
+			.set($birdEyes, { autoAlpha: 1 }, '+=0.2')
+			.add('bird-blinks')
+			.to($bird, 0.8, { y: '-=34', ease: Power4.easeInOut })
+			.to($bird, 0.3, { y: '+=8', ease: Power4.easeInOut })
+			.to($birdHat, 0.4, { y: '-=12' }, '-=0.6')
+			.to($birdHat, 0.3, { y: 0, rotation: 0, x: 0 })
+
+		return treeStuffTl;
+	}
 	// enter the greeting text
 
 	// the GO function ...to kick things all off
@@ -51,6 +85,8 @@
 
 		masterTl
 			.add(clearStage(), 'scene-clear-stage')
+			.add(enterFloorVegetation(), 'scene-floor-vegetation')
+			.add(enterTreestuff(), 'scene-enter-treestuff')
 	}
 
 	go();
